82c614eab1c47112fe4a4cb1615e199270b80f53
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-19 19:59:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTime() {
+			let now = new Date();
+			let timeString = now.toLocaleTimeString();
+			let dateString = now.toLocaleDateString();
+			document.getElementById("timer").innerHTML =  dateString+","+ timeString ;
+		}
+
+		setInterval(updateTime, 1000);
